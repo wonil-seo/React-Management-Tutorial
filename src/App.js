7a5a8f9433586ae159b4ad7e28958610a896ed2b
seowonil <wiseo@insunginfo.co.kr>
2020-11-10 15:34:35 +0900

    map sample

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': 'wonil-seo',
+  'birthday': '1111',
+  'gender': 'male',
+  'job': 'student'  
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': 'wonil-aaa',
+    'birthday': '3333',
+    'gender': 'female',
+    'job': 'student'  
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': 'wonil-bbb',
+    'birthday': '5555',
+    'gender': 'male',
+    'job': 'student'  
+    }
+];
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer key={c.id}
+                id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
