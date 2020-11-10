1032b3e6ee4797f3e451e2ad59de86095e1dca69
seowonil <wiseo@insunginfo.co.kr>
2020-11-10 19:29:23 +0900

    customer get server data

--- client/src/App.js
+++ client/src/App.js
@@ -20,34 +20,23 @@ const styles = theme => ({
   }
 });
 
-const customers = [
-  {
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': 'wonil-seo',
-  'birthday': '1111',
-  'gender': 'male',
-  'job': 'student'  
-  },
-  {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name': 'wonil-aaa',
-    'birthday': '3333',
-    'gender': 'female',
-    'job': 'student'  
-  },
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name': 'wonil-bbb',
-    'birthday': '5555',
-    'gender': 'male',
-    'job': 'student'  
-    }
-];
-
 class App extends Component {
+  state = {
+    customers: ""
+  }
+
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async() => {
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    return body;
+  }
+
   render(){
     const {classes} = this.props;
     return (
@@ -65,12 +54,12 @@ class App extends Component {
           </TableHead>
           <TableBody>
             {
-              customers.map(c => {
+              this.state.customers ? this.state.customers.map(c => {
                 return (
                   <Customer key={c.id}
                     id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>
                 );
-              })
+              }) : ""
             }
           </TableBody>
         </Table>
--- server.js
+++ server.js
@@ -6,8 +6,34 @@ const port = process.env.port || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/api/hello", (req, res) => {
-    res.send({message: "Hello Express!"});
+app.get("/api/customers", (req, res) => {
+    res.send([
+        {
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/1',
+        'name': 'wonil-seo',
+        'birthday': '1111',
+        'gender': 'male',
+        'job': 'student'  
+        },
+        {
+            'id': 2,
+            'image': 'https://placeimg.com/64/64/2',
+            'name': 'wonil-aaa',
+            'birthday': '2222',
+            'gender': 'female',
+            'job': 'student'  
+        },
+        {
+            'id': 3,
+            'image': 'https://placeimg.com/64/64/3',
+            'name': 'wonil-bbb',
+            'birthday': '3333',
+            'gender': 'male',
+            'job': 'student'  
+        }
+    ]);
+
 });
 
 app.listen(port, () => {
